121b029e593d45da663756af0c3736b8960c1274
jellysoo97 <jellysoo97@gmail.com>
2024-01-31 00:04:55 +0900

    feat: create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 
-import { Cutive_Mono, Nanum_Myeongjo } from 'next/font/google'
 import React from 'react'
 
 import Footer from '@/components/layout/Footer'
@@ -8,31 +7,18 @@ import Header from '@/components/layout/Header'
 import NextThemeProvider from '@/components/provider/NextThemeProvider'
 import ViewProvider from '@/components/provider/ViewProvider'
 
-const serif = Nanum_Myeongjo({
-  subsets: ['latin'],
-  weight: '400',
-  display: 'swap',
-  variable: '--font-serif',
-})
-const mono = Cutive_Mono({
-  subsets: ['latin'],
-  weight: '400',
-  display: 'swap',
-  variable: '--font-mono',
-})
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${serif.variable} ${mono.variable}`}>
+    <html lang="en">
       <body>
         <NextThemeProvider>
           <ViewProvider>
             <Header />
-            <main>{children}</main>
+            <main className="wrapper h-screen">{children}</main>
             <Footer />
           </ViewProvider>
         </NextThemeProvider>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,17 @@
+import { siteConfig } from '@/constants/config'
+import { DateFormatTypeEnum, formatDate, today } from '@/utils/format-date'
+
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <div className="w-full border-t border-neutral-400">
+        <div className="wrapper flex justify-center items-center py-4 text-sm">
+          Copyright © {formatDate(today, DateFormatTypeEnum.YearOnly)}{' '}
+          {siteConfig.author.name} All rights reserved
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
