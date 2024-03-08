b2060b3d27b81e0a1122eefda8d428e2339bd6b6
jellysoo97 <jellysoo97@gmail.com>
2024-03-08 21:45:53 +0900

    feat: create common badge component

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,5 +1,24 @@
-const Badge = () => {
-  return <div>badge</div>
+import React from 'react'
+
+import { cn } from '@/utils/cn'
+
+type Props = {
+  text: string
+} & React.ComponentProps<'div'>
+
+const Badge = ({ text, className, ...props }: Props) => {
+  return (
+    <div
+      className={cn(
+        'flex justify-center items-center px-2 py-1 rounded-md border-2 border-neutral-300 dark:border-neutral-700',
+        'cursor-pointer',
+        className
+      )}
+      {...props}
+    >
+      {text}
+    </div>
+  )
 }
 
 export default Badge
