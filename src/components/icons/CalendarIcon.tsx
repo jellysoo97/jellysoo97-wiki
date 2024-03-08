b3dd73398267af5a2ecf3699b2754cabaead4c5b
jellysoo97 <jellysoo97@gmail.com>
2024-03-08 21:50:14 +0900

    styles: fix styles

--- src/components/icons/CalendarIcon.tsx
+++ src/components/icons/CalendarIcon.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 
+import { cn } from '@/utils/cn'
+
 const CalendarIcon = ({ className, ...props }: React.ComponentProps<'svg'>) => {
   return (
     <svg
-      className={className}
-      aria-hidden="true"
+      className={cn('w-6 h-6', className)}
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14c.6 0 1-.4 1-1V7c0-.6-.4-1-1-1H5a1 1 0 0 0-1 1v12c0 .6.4 1 1 1Z"
-      />
+      <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+      <line x1="16" y1="2" x2="16" y2="6"></line>
+      <line x1="8" y1="2" x2="8" y2="6"></line>
+      <line x1="3" y1="10" x2="21" y2="10"></line>
     </svg>
   )
 }
