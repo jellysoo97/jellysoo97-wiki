ec146dc6f870f13d4c4741d50f24568f2ca0a62b
jellysoo97 <jellysoo97@gmail.com>
2024-03-09 22:31:22 +0900

    feat: create new icons

---
+++ src/components/icons/CircleIcon.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { cn } from '@/utils/cn'
+
+const ArrowRightIcon = ({
+  className,
+  ...props
+}: React.ComponentProps<'svg'>) => {
+  return (
+    <svg
+      className={cn('w-6 h-6', className)}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+    </svg>
+  )
+}
+
+export default ArrowRightIcon
