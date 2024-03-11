8c6e72faa50930936217cad412e7a3834bcbcc9c
jellysoo97 <jellysoo97@gmail.com>
2024-03-11 23:59:26 +0900

    chore: add banner config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,4 +8,8 @@ export const siteConfig = {
   url: {
     github: 'https://github.com/jellysoo97/jellysoo97-playground',
   },
+  banner: {
+    img: 'https://i.pinimg.com/564x/a0/38/97/a03897ad7fc9a40824ecba186b5ca14d.jpg',
+    pixelSize: 50,
+  },
 }
