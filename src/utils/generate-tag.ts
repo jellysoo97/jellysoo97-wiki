c003c2304bf876c2351850921d3398f4206935e9
jellysoo97 <jellysoo97@gmail.com>
2024-03-25 20:47:15 +0900

    chore: remove unnecessary components and utils

--- src/utils/generate-tag.ts
+++
@@ -1,6 +0,0 @@
-import { CategoryEnum } from '@/components/icons/CategoryIcon'
-import { categoryText } from '@/constants/category'
-
-export const generateTag = (part: string, category: string): string[] => {
-  return [part, categoryText[category as CategoryEnum]].map((tag) => `#${tag}`)
-}
