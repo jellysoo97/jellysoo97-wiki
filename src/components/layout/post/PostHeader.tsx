d649cef3b3fb3488a9df55201c3ea83591776d96
jellysoo97 <jellysoo97@gmail.com>
2024-03-29 19:35:17 +0900

    fix: fix post header

--- src/components/layout/post/PostHeader.tsx
+++ src/components/layout/post/PostHeader.tsx
@@ -1,7 +1,6 @@
 import { Post } from 'contentlayer/generated'
 
 import Title from '@/components/common/Title'
-import { CategoryEnum, categoryKR, PartEnum, partKR } from '@/constants/menus'
 import { DateFormatTypeEnum, formatDate } from '@/utils/format-date'
 
 type Props = {
@@ -10,23 +9,11 @@ type Props = {
 
 const PostHeader = ({ post }: Props) => {
   return (
-    <div className="flex flex-col mt-4 gap-y-1">
+    <div className="flex flex-col mt-4">
+      <p className="text-secondary text-size-small">
+        {formatDate(post.date, DateFormatTypeEnum.DateOnlyWithDot)}
+      </p>
       <Title>{post.title}</Title>
-      {!!post.description && (
-        <span className="text-secondary text-size-small">
-          {post.description}
-        </span>
-      )}
-      <div className="flex items-center gap-x-2">
-        <span className="text-secondary text-size-small">
-          {partKR[post.part as PartEnum]} {'>'}{' '}
-          {categoryKR[post.category as CategoryEnum]}
-        </span>
-        |
-        <span className="text-size-small text-secondary">
-          {formatDate(post.date, DateFormatTypeEnum.DateOnlyWithDot)}
-        </span>
-      </div>
     </div>
   )
 }
