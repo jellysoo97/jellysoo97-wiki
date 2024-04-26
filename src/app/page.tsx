7ce43bff455f05cb1e0ce66b70e41c824734d7cf
jellysoo97 <jellysoo97@gmail.com>
2024-04-27 03:05:15 +0900

    feat: create post grape component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 import BarGraph, { BarGraphData } from '@/components/common/BarGraph'
 import UnderlineLink from '@/components/common/UnderlineLink'
-import PixelBanner from '@/components/PixelBanner'
-import { DEFAULT_TAG_COLOR } from '@/constants/menus'
+import PostGrape from '@/components/PostGrape'
+import { CategoryEnum, DEFAULT_TAG_COLOR } from '@/constants/menus'
 import { allCategories, allSortedPosts, menuTags } from '@/constants/posts'
 import { calculatePercentage } from '@/utils/calculate-percentage'
 
@@ -20,19 +20,25 @@ export default function HomePage() {
   return (
     <div className="w-full flex flex-col gap-y-8">
       <section className="flex gap-x-4">
-        <PixelBanner />
-        <div className="flex flex-col flex-1 gap-y-4 md:justify-between">
-          <Link href={''}>@jellysoo97</Link>
+        <PostGrape />
+        <div className="flex flex-col flex-1 justify-between gap-y-4">
+          <p>@jellysoo97</p>
           <BarGraph data={graphData} />
         </div>
       </section>
 
       <section>
+        <Link href={`/${CategoryEnum.All}`}>
+          <p className="text-primary text-size-base px-2 py-1 bg-secondary mb-4">
+            📌 &nbsp;{allCategories[0].valueKR}&nbsp;(
+            {allCategories[0].postCount})
+          </p>
+        </Link>
         <div className="flex flex-col gap-4 md:grid grid-cols-2">
           {allCategories.slice(1).map((category) => (
             <div key={category.value} className="flex flex-col gap-y-2">
               <p className="text-primary text-size-base px-2 py-1 bg-secondary">
-                📌 &nbsp;{category.valueKR}
+                📌 &nbsp;{category.valueKR}&nbsp;({category.postCount})
               </p>
               <ul>
                 {menuTags
---
+++ src/components/PostGrape.tsx
@@ -0,0 +1,91 @@
+import { allPosts } from 'contentlayer/generated'
+import Link from 'next/link'
+import { Fragment } from 'react'
+
+import { allSortedPosts } from '@/constants/posts'
+import { cn } from '@/utils/cn'
+
+import Tooltip from './common/Tooltip'
+
+const PostGrape = () => {
+  const postCount = allPosts.length
+  const postItems = [
+    [0, 1, 2],
+    [3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20],
+    [21, 22, 23],
+    [24, 25, 26],
+    [27, 28],
+    [29],
+  ]
+
+  return (
+    <div className="flex flex-col items-center justify-center px-8 py-4 border-secondary md:px-12">
+      <div className="flex">
+        {/* 잎 */}
+        <div
+          className={cn('w-8 h-8 bg-green-600 -ml-10')}
+          style={{ borderRadius: '0 90px' }}
+        />
+        <div
+          className={cn('w-8 h-8 bg-green-500 -ml-10 mt-2 rotate-[330deg]')}
+          style={{ borderRadius: '0 90px' }}
+        />
+
+        {/* 줄기 */}
+        <div className="w-2 h-6 rounded-lg bg-yellow-900 mt-[14px] ml-3" />
+      </div>
+
+      <div className="flex flex-col">
+        {postItems.map((posts, rowIndex) => (
+          <div
+            key={rowIndex}
+            className={cn(
+              'flex items-center gap-x-[2px]',
+              'ml-6',
+              rowIndex === 1 && 'ml-1',
+              rowIndex === 8 && 'ml-8',
+              rowIndex > 1 && rowIndex < 8 && rowIndex % 2 === 0 && 'ml-4',
+              rowIndex > 1 && rowIndex < 8 && rowIndex % 2 !== 0 && 'ml-6'
+            )}
+          >
+            {posts.map((postIndex, colIndex) => {
+              const isFilledPost = postIndex < postCount
+
+              return (
+                <Fragment key={`${rowIndex}-${colIndex}`}>
+                  {isFilledPost && (
+                    <Tooltip content={<p>{allSortedPosts[postIndex].title}</p>}>
+                      <div
+                        className={cn(
+                          'w-4 h-4 rounded-full border border-purple-500 dark:border-purple-400',
+                          'bg-purple-400 dark:bg-purple-500 hover:scale-125'
+                        )}
+                      >
+                        <Link
+                          href={allSortedPosts[postIndex].url}
+                          className="w-full h-full block"
+                        />
+                      </div>
+                    </Tooltip>
+                  )}
+
+                  {!isFilledPost && (
+                    <div
+                      key={`${rowIndex}-${colIndex}`}
+                      className={cn('w-4 h-4 rounded-full border-primary')}
+                    />
+                  )}
+                </Fragment>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default PostGrape
