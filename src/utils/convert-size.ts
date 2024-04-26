c9c3426ac94c4977533a475438644ceaac5443e3
jellysoo97 <jellysoo97@gmail.com>
2024-04-26 23:57:16 +0900

    chore: remove unused util

--- src/utils/convert-size.ts
+++
@@ -1,9 +0,0 @@
-const DEFAULT_FONT_SIZE = 16
-
-export const convertSize = (size: number, unit?: 'px' | 'rem') => {
-  if (unit === 'rem') {
-    return `${(size / DEFAULT_FONT_SIZE).toFixed(2)}rem`
-  }
-
-  return `${size}px`
-}
