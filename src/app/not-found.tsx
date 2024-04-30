791c083b4b3b10be35c9344db726f97e7f844e1e
jellysoo97 <jellysoo97@gmail.com>
2024-05-01 01:32:44 +0900

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <section className="w-full flex flex-col justify-center items-center my-40">
+      <h1 className="font-serif-bold text-2xl">404</h1>
+      <h1 className="font-serif-bold text-xl">존재하지 않는 페이지입니다🥲</h1>
+    </section>
+  )
+}
+
+export default NotFoundPage
