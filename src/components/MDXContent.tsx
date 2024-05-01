fd3ff89907e11452ca7b29fdcd7fabe6b0f1720f
jellysoo97 <jellysoo97@gmail.com>
2024-05-02 00:35:35 +0900

    feat: add code copy logic to pre component

--- src/components/MDXContent.tsx
+++ src/components/MDXContent.tsx
@@ -1,5 +1,13 @@
+'use client'
+
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import React from 'react'
+import { useRef, useState } from 'react'
+
+import { useTimeOut } from '@/hooks/use-time-out'
+
+import IconButton from './common/IconButton'
+import CheckIcon from './icons/CheckIcon'
+import CopyIcon from './icons/CopyIcon'
 
 type Props = {
   code: string
@@ -9,16 +17,40 @@ const img = ({ src, alt, ...props }: React.ComponentProps<'img'>) => {
   return <img src={src || ''} alt={alt || 'image'} {...props} />
 }
 
-const pre = ({ title, children }: React.ComponentProps<'pre'>) => {
-  return (
-    <div>
-      {title && <>{title}</>}
+const pre = ({ children }: React.ComponentProps<'pre'>) => {
+  const preRef = useRef<HTMLPreElement>(null)
+  const [isCopied, setIsCopied] = useState<boolean>(false)
 
-      <pre className="relative">
-        {children}
-        {/* copy button */}
-      </pre>
-    </div>
+  useTimeOut(isCopied, 1500, () => setIsCopied(false))
+
+  const handleCodeCopy = async () => {
+    const code = preRef.current?.querySelector('code')?.innerText
+
+    if (!code) {
+      return
+    }
+
+    try {
+      await navigator.clipboard.writeText(code)
+      setIsCopied(true)
+    } catch (err) {
+      console.log(err)
+      alert('코드 복사에 실패했습니다.')
+    }
+  }
+
+  return (
+    <pre ref={preRef} className="relative">
+      <IconButton className="absolute top-2 right-2" onClick={handleCodeCopy}>
+        {isCopied ? (
+          <CheckIcon className="w-6 h-6 text-green-700" />
+        ) : (
+          <CopyIcon className="w-6 h-6 text-secondary" />
+        )}
+      </IconButton>
+
+      {children}
+    </pre>
   )
 }
 
