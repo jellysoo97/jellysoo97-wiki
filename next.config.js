dec374e41f98f02b1fe10e9ea7bafdb8923beaac
jellysoo97 <jellysoo97@gmail.com>
2024-05-26 23:19:04 +0900

    fix: fix contentlayer build dependencies failed warning

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withContentlayer } = require('next-contentlayer')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withContentlayer(nextConfig)
