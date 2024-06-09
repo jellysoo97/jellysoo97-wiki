1c352eee9924d201508f5cbb59efbbbab8030067
jellysoo97 <jellysoo97@gmail.com>
2024-06-09 22:22:29 +0900

    feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,88 +23,64 @@ const AboutPage = () => {
     <div className="w-full flex flex-col gap-y-8">
       <section className="flex flex-col gap-y-2">
         <Title>박소연</Title>
-        <div className="flex justify-between items-center">
-          <p className="text-secondary">Web Frontend Developer</p>
-          <div className="flex gap-x-2">
-            <Tooltip content={siteConfig.url.author}>
-              <a
-                href={siteConfig.url.author}
-                target="_blank"
-                className="underline underline-offset-2"
-                rel="noreferrer"
-              >
-                Github
-              </a>
-            </Tooltip>
-            |
-            <Tooltip
-              content={
-                isCopied ? COPIED_TOOLTIP_CONTENT : siteConfig.author.email
-              }
+        <p className="text-secondary">Web Frontend Developer</p>
+
+        <Divider />
+      </section>
+
+      <section className="flex flex-col gap-y-2">
+        <div className="flex gap-x-2">
+          <p className="inline-block w-[50px]">Github</p> |
+          <a
+            href={siteConfig.url.author}
+            target="_blank"
+            className="underline underline-offset-2"
+            rel="noreferrer"
+          >
+            {siteConfig.url.author}
+          </a>
+        </div>
+        <div className="flex gap-x-2">
+          <p className="inline-block w-[50px]">Blog</p> |
+          <a
+            href={siteConfig.url.blog}
+            target="_blank"
+            className="underline underline-offset-2"
+            rel="noreferrer"
+          >
+            {siteConfig.url.blog}
+          </a>
+        </div>
+        <div className="flex gap-x-2">
+          <p className="inline-block w-[50px]">Email</p> |
+          <Tooltip content={isCopied ? COPIED_TOOLTIP_CONTENT : ''}>
+            <span
+              className="underline underline-offset-2 cursor-pointer"
+              onClick={handleCopy}
             >
-              <span
-                className="underline underline-offset-2 cursor-pointer"
-                onClick={handleCopy}
-              >
-                Email
-              </span>
-            </Tooltip>
-            |
-            <Tooltip content={siteConfig.url.blog}>
-              <a
-                href={siteConfig.url.blog}
-                target="_blank"
-                className="underline underline-offset-2"
-                rel="noreferrer"
-              >
-                Blog
-              </a>
-            </Tooltip>
-          </div>
+              {siteConfig.author.email}
+            </span>
+          </Tooltip>
         </div>
-
-        <Divider />
       </section>
 
       <section className="flex flex-col gap-y-2">
-        <Title>About</Title>
-        <Divider />
+        <strong className="font-serif-bold">
+          서비스가 어떻게 동작하는 지 알고 설계대로 동작하게 만드는 개발을
+          지향합니다.
+        </strong>
         <p>
-          <strong>서비스</strong>
+          근거 있는 선택을 하려고 노력합니다. 최선의 선택을 위해 고민한 과정과
+          느낀점을 동료들에게 공유하고 피드백을 주고받는 과정을 즐깁니다.
         </p>
-
-        <h2 className="text-lg font-serif-bold">Skills</h2>
-        <ul className="parent-ul">
-          <li>TypeScript, React, Next.js</li>
-          <li>styled-components, Emotion, TailwindCSS</li>
-          <li>Node.js, Express</li>
-        </ul>
-      </section>
-
-      <section className="flex flex-col gap-y-2">
-        <Title>Work Experience</Title>
-        <Divider />
-        <div className="grid grid-cols-[200px_minmax(500px,_1fr)]">
-          <p className="text-secondary">2022.09 ~ 2023.11</p>
-          <div className="flex flex-col gap-y-2">
-            <h2 className="text-lg font-serif-bold text-yellow">
-              프리디소프트
-            </h2>
-            <p>1. 유찜 프로젝트</p>
-            <ul>
-              <li></li>
-            </ul>
-            <p>2. 하모니 프로젝트</p>
-            <ul>
-              <li></li>
-            </ul>
-          </div>
-        </div>
-      </section>
-
-      <section className="flex flex-col gap-y-2">
-        <Title>Projects</Title>
-        <Divider />
+        <br />
+        <strong className="font-serif-bold">
+          기본기를 중요하게 생각합니다.
+        </strong>
+        <p>
+          빠르게 변하는 프론트엔드 생태계에 유연하게 적응하고 다양한 도메인
+          경험을 쌓기 위해 로우 레벨에 관심을 가지고 공부하고 있습니다.
+        </p>
       </section>
     </div>
   )
--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -24,20 +24,22 @@ const Tooltip = ({ children, position = 'top', content }: Props) => {
 
       {/* TODO: add border triangles */}
       {/* https://codesandbox.io/p/sandbox/how-to-make-an-extremely-reusable-tooltip-component-with-react-and-nothing-else-7opo3?file=%2Fsrc%2FTooltip.css%3A9%2C15 */}
-      <div
-        className={cn(
-          'hidden',
-          // TODO: 1. replace static position to dynamic position
-          // TODO: 2. add position
-          // {/* https://gist.github.com/ndpniraj/633474d23145499c5a3c39b017f43be4 */}
-          `absolute`,
-          'px-[6px] py-1 text-neutral-700 bg-yellow rounded-md text-size-small z-30 whitespace-nowrap',
-          'group-hover:block'
-        )}
-        style={tooltipPosition[position]}
-      >
-        {content}
-      </div>
+      {!!content && (
+        <div
+          className={cn(
+            'hidden',
+            // TODO: 1. replace static position to dynamic position
+            // TODO: 2. add position
+            // {/* https://gist.github.com/ndpniraj/633474d23145499c5a3c39b017f43be4 */}
+            `absolute`,
+            'px-[6px] py-1 text-neutral-700 bg-yellow rounded-md text-size-small z-30 whitespace-nowrap',
+            'group-hover:block'
+          )}
+          style={tooltipPosition[position]}
+        >
+          {content}
+        </div>
+      )}
     </div>
   )
 }
