c1afe5fa96c991147f0005bf4db3d57f3816628a
jellysoo97 <jellysoo97@gmail.com>
2024-06-09 15:14:58 +0900

    chore: remove about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,8 @@
-'use client'
-
-import Experience from '@/components/about/Experience'
-import Profile from '@/components/about/Profile'
-
 const AboutPage = () => {
   return (
-    <div className="relative w-full flex flex-col gap-y-8">
-      {/* TODO: navigator */}
-
-      {/* first section */}
-      <section className="min-h-[var(--height-main)]">
-        <Profile />
-      </section>
-
-      {/* second section */}
-      <section className="min-h-[var(--height-main)]">
-        <Experience />
+    <div className="flex flex-col">
+      <section>
+        <p>준비 중입니다.</p>
       </section>
     </div>
   )
