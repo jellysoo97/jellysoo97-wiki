71ec9f78dfcf1eb859448eb58e6381d655d4fdbd
jellysoo97 <jellysoo97@gmail.com>
2024-10-10 21:49:49 +0900

    chore: setup SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,38 @@ import BarGraph, { BarGraphData } from '@/components/common/BarGraph'
 import Title from '@/components/common/Title'
 import PostList from '@/components/posts/PostList'
 import { calculatePercentage } from '@/utils/calculate-percentage'
-import { getMdxData } from '@/utils/get-mdx-data'
 
+import { siteConfig } from '@/constants/config'
+import { getPosts } from '@/utils/get-posts'
+import { getTags } from '@/utils/get-tags'
+import { Metadata } from 'next'
 import { DEFAULT_TAG_COLOR } from '../constants'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const DEFAULT_IMAGE = '/images/og.png'
+  const { title, description, author, url } = siteConfig
+  const posts = await getPosts()
+  const tags = getTags(posts).map((tag) => tag.label)
+
+  return {
+    title,
+    description,
+    authors: author,
+    keywords: tags,
+    openGraph: {
+      type: 'website',
+      title,
+      description,
+      url: url.blog,
+      images: [DEFAULT_IMAGE],
+    },
+  }
+}
 
 export default async function HomePage() {
   // https://stackoverflow.com/questions/64926174/module-not-found-cant-resolve-fs-in-next-js-application
-  const { posts, tags } = await getMdxData()
+  const posts = await getPosts()
+  const tags = getTags(posts)
   const graphData: BarGraphData[] = tags.map((tag) => ({
     item: tag.label,
     percentage: calculatePercentage({
